49c9aaf99ae182805138a86a974ffcb07dfdb638
Bassel Raafat <basselr11@gmail.com>
2025-05-03 02:42:09 +0300

    Migratinos

--- src/ChatApp.Repository/Data/Migrations/20250502174031_Changed the user entity.Designer.cs
+++ src/ChatApp.Repository/Data/Migrations/20250502174031_Changed the user entity.Designer.cs
@@ -4,6 +4,7 @@ using ChatApp.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChatApp.Repository.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250502174031_Changed the user entity")]
+    partial class Changedtheuserentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
